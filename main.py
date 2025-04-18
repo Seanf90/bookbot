3fc1ce817f00172355df27b063b0ccf4b1fc88ca
Seanf90 <seanxf@hotmail.co.uk>
2025-04-18 15:36:20 +0100

    Added new books, a printed report detailed num of words, and a sorted list of characters.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from stats import num_of_words, num_of_characters
+from stats import num_of_words, num_of_characters, sort_dictionary, generate_report
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         book_contents = f.read()
     return book_contents
 
+def print_full_report(filepath, num_words, sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    generate_report(sorted_characters)
+    print("============= END ===============")
+
+
 def main():
-    contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    contents = get_book_text(filepath)
     num_words = num_of_words(contents)
-    print(f"{num_words} words found in the document")
-    print(num_of_characters(contents))
+    characters = num_of_characters(contents)
+    sorted_characters = sort_dictionary(characters)
+    print_full_report(filepath, num_words, sorted_characters)
+
 main()
-
-
-    
--- stats.py
+++ stats.py
@@ -5,10 +5,10 @@ def num_of_words(book_contents):
         count += 1
     return count
 
+
 def num_of_characters(book_contents):
     book_contents = book_contents.lower()
     character_count = {}
-
     characters = list(book_contents)
 
     for c in characters:
@@ -17,5 +17,21 @@ def num_of_characters(book_contents):
     for c in characters:
         if c in character_count:
             character_count[c] += 1
-
     return character_count
+
+
+def sort_on(char_dict):
+    return char_dict["count"]
+
+def sort_dictionary(character_count):
+    character_list = []
+    for char, count in character_count.items():
+        character_list.append({"char": char, "count": count})
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+
+def generate_report(character_list):
+    for char in character_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["count"]}")
